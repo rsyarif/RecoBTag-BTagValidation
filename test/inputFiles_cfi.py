faafd341b72a593c0685197b766062731e80178c
ferencek <>
2013-05-20 00:02:12 +0000

    initial commit

---
+++ test/inputFiles_cfi.py
@@ -0,0 +1,4 @@
+FileNames = [
+    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/performance/CMSSW_5_3_9/MC/QCD_Pt-300to470_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v2/JetTree_mc_subjets_1_1_Lpb.root',
+    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/performance/CMSSW_5_3_9/MC/QCD_Pt-300to470_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v2/JetTree_mc_subjets_2_1_EHF.root'
+]
