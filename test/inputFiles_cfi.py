c95391807b1a8561699ee29bce8b3cf808bf47b9
Devdatta <devdatta.majumder@cern.ch>
2015-05-27 11:57:32 +0200

    recommitting

---
+++ test/inputFiles_cfi.py
@@ -0,0 +1,6 @@
+FileNames = [
+    'file:/afs/cern.ch/work/d/devdatta/CMSREL/CMSSW_7_4_2/src/RecoBTag/PerformanceMeasurements/test/BTagAnalyzerMacros/JetTree_mc_subjets_9.root'
+    #'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/performance/CMSSW_5_3_11/Data/JetHT/Run2012B-22Jan2013-v1/JetTree_data_subjets_100_1_Le3.root',
+    #'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/performance/CMSSW_5_3_11/MC/QCD_Pt-470to600_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/JetTree_mc_subjets_10_1_pDK.root'
+    #'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/performance/CMSSW_5_3_11/MC/TTJets_HadronicMGDecays_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A_ext-v1/JetTree_mc_subjets_100_1_j4K.root'
+]
