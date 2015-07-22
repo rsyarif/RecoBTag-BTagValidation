f668cf0bff3623c582e674c281ee4fa63230d904
rizki <rizki_syarif@brown.edu>
2015-07-22 10:40:55 -0500

    fixed typo

--- test/sandbox/condor_jobs/mergeQCDMuEnriched.sh
+++ test/sandbox/condor_jobs/mergeQCDMuEnriched.sh
@@ -5,5 +5,5 @@ mkdir  QCDMuEnriched_ptmin-300_70m200_muTag_tau21-05
 mkdir  QCDMuEnriched_ptmin-300_70m200_muTag
 
 python ../../mergeDatasets.py -w /eos/uscms/store/user/rsyarif/HbbTagVal/EventSelectionStudies/ -d datasetListForMerging_QCDMuEnriched_ptmin-300_70m200_muTag_tau21-05.txt -o QCDMuEnriched_ptmin-300_70m200_muTag_tau21-05
-python ../../mergeDatasets.py -w /eos/uscms/store/user/rsyarif/HbbTagVal/EventSelectionStudies/ -d datasetListForMerging_QCDMuEnriched_ptmin-300_70m200_muTag_tau21-05.txt -o QCDMuEnriched_ptmin-300_70m200_muTag_tau21-04
+python ../../mergeDatasets.py -w /eos/uscms/store/user/rsyarif/HbbTagVal/EventSelectionStudies/ -d datasetListForMerging_QCDMuEnriched_ptmin-300_70m200_muTag_tau21-04.txt -o QCDMuEnriched_ptmin-300_70m200_muTag_tau21-04
 python ../../mergeDatasets.py -w /eos/uscms/store/user/rsyarif/HbbTagVal/EventSelectionStudies/ -d datasetListForMerging_QCDMuEnriched_ptmin-300_70m200_muTag.txt -o QCDMuEnriched_ptmin-300_70m200_muTag
