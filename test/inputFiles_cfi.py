9028533c2bdb26c3a95b81c9d0c2d404c1dc2249
Devdatta <devdatta.majumder@cern.ch>
2015-10-27 21:08:01 +0100

    adding PU reweighting histograms and cleaning up inputFiles_cfi.py

--- test/inputFiles_cfi.py
+++ test/inputFiles_cfi.py
@@ -1,12 +1,2 @@
 FileNames = [
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_1.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_2.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_3.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_4.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_5.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_6.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_7.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_8.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_9.root',
-    'root://eoscms.cern.ch//eos/cms/store/group/phys_btag/BoostedBTag/7_4_X_25ns.00/JetHT-2015D-PromptReco-v3/JetTree_data_FatJets_Subjets_BoostedCommissioning_10.root'
 ]
